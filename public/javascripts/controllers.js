b61070db61c48d66479582198b06d4a8901e43bc
Joe D. Bentley <joebentley10@gmail.com>
2014-07-23 20:23:58 +0100

    Handled bad names

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -14,10 +14,18 @@ app.controller('itemController', function ($scope, $routeParams, searchFactory) 
     var result = $.grep(results, function (object) { return object.name === $routeParams.itemName; });
 
     // Check that item was found
-    if (result.length == 0) {
-      // not found
-    } else if (result.length == 1) {
-      $scope.item = result[0]
+    if (result.length === 0) {
+      $scope.itemExists = false;
+
+      // Set name for error message
+      $scope.badName = $routeParams.itemName;
+
+    } else if (result.length === 1) {
+      // Item was found
+      $scope.itemExists = true;
+
+      // Set current item to result
+      $scope.item = result[0];
     }
   });
 });
